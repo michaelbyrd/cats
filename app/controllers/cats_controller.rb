3de4deedc198a6ed8578d607a3a6bd747b7bfc6a
MichaelByrd <michaelbyrdsemail@gmail.com>
2014-07-23 11:58:33 -0400

    add testing for :create, :update, :destroy on cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,8 +1,8 @@
 class CatsController < ApplicationController
   respond_to :json
+  before_action :set_cat, only: [:show, :update, :destroy]
 
   def show
-    @cat = Cat.find(params[:id])
     respond_with @cat
   end
 
@@ -11,4 +11,29 @@ class CatsController < ApplicationController
     respond_with @cats
   end
 
+  def update
+    @cat.update(cat_params)
+    respond_with @cat
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    @cat.save
+    respond_with @cat
+  end
+
+  def destroy
+    @cat.destroy
+    respond_with @cat
+  end
+
+  private
+
+  def set_cat
+    @cat = Cat.find(params[:id])
+  end
+
+  def cat_params
+    params.require(:cat).permit!
+  end
 end
--- spec/controllers/cats_controller_spec.rb
+++ spec/controllers/cats_controller_spec.rb
@@ -10,13 +10,40 @@ describe CatsController do
     expect(JSON.parse(response.body)).not_to be_empty
     expect(data.first['name']).to eq cats.first.name
   end
+
   it 'GET :show' do
     cat = create(:cat)
     get :show, id: cat.id, format: :json
     data = JSON.parse(response.body)
     expect(data).to have_key('name')
   end
-  it 'POST :create'
-  it 'PATCH :update'
-  it 'DELETE :destroy'
+  describe 'POST :create' do
+    before {@cat_attributes = attributes_for(:cat)}
+    it 'succeeds' do
+      post :create, cat: @cat_attributes, format: :json
+      expect(response).to have_http_status(:created) # created
+      data = JSON.parse(response.body)
+      expect(data).to have_key('name')
+      expect(data['name']).to eq cat_attributes[:name]
+    end
+
+    it 'fails when a field is empty' do
+      @cat_attributes[:name] = nil
+      post :create, cat: @cat_attributes, format: :json
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+  it 'PATCH :update' do
+    cat = create(:cat)
+    patch :update, id: cat.id, cat: {name: 'Jasmine'}, format: :json
+    expect(response).to have_http_status(:no_content)
+    expect(Cat.find(cat.id).name).to eq 'Jasmine'
+  end
+
+  it 'DELETE :destroy' do
+    cat = create(:cat)
+    delete :destroy, id: cat.id, format: :json
+    expect(response).to have_http_status(:no_content)
+    expect{Cat.find(cat.id)}.to raise_error ActiveRecord::RecordNotFound
+  end
 end
